e8991f09bf7a6c028357edc86cbdc868a13833f8
Moises Santos <moisesbarbosa23@gmail.com>
2024-03-05 23:01:31 -0300

    feat(pagamentos): falso delay para simular o processamento do pagamento.

--- apps/pagamentos/src/main/java/org/ecommerce/pagamentos/service/TransacaoService.java
+++ apps/pagamentos/src/main/java/org/ecommerce/pagamentos/service/TransacaoService.java
@@ -1,10 +1,14 @@
 package org.ecommerce.pagamentos.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.ecommerce.pagamentos.domain.Transacao;
 import org.ecommerce.pagamentos.enums.StatusPagamento;
 import org.ecommerce.pagamentos.rabbitmq.QueueSender;
 import org.ecommerce.pagamentos.repository.TransacoesRepository;
 import org.ecommerce.pedidos.dtos.PedidoDTO;
+import org.ecommerce.pedidos.enums.StatusPedido;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,8 @@ public class TransacaoService {
 
     private final TransacoesRepository transacoesRepository;
     private final QueueSender queueSender;
+
+    private final Integer UM_MINUTO = 60000;
 
     public TransacaoService(TransacoesRepository transacoesRepository, QueueSender queueSender) {
         this.transacoesRepository = transacoesRepository;
@@ -23,21 +29,34 @@ public class TransacaoService {
                 pedidoDTO.getIdCliente(),
                 pedidoDTO.getId(),
                 pedidoDTO.getIdCarrinho(),
-                StatusPagamento.APROVADO,
+                StatusPagamento.PENDENTE,
                 pedidoDTO.getValorTotal()
         );
 
         try {
             transacoesRepository.save(transacao);
 
-            enviarNotificacao(pedidoDTO.getIdCliente());
+            System.out.println("Processando transação...");
+            Thread.sleep(UM_MINUTO);
+            System.out.println("Transação processada");
+            transacao.setStatus(StatusPagamento.APROVADO);
+            transacoesRepository.save(transacao);
+            pedidoDTO.setStatus(StatusPedido.PAGAMENTO_APROVADO);
+            enviarNotificacao(pedidoDTO);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void enviarNotificacao(Long idCliente) {
-        queueSender.send("Notificação de pagamento", "notificacoes");
+    public void enviarNotificacao(PedidoDTO pedidoDTO) {
+
+        try {
+            System.out.println("Enviando email de pagamento");
+            ObjectMapper mapper = new ObjectMapper();
+            queueSender.send(mapper.writeValueAsString(pedidoDTO), "notificacoes");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         // Enviar notificação para o cliente
     }
 
